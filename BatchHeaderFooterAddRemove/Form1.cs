929d3a05dfb0cc729b141851c5dab947ad3f315a
SmilingAngelsTea <smilingangelstea@hotmail.com>
2020-01-04 17:44:52 +0300

    Code and LICENSE.md push

---
+++ BatchHeaderFooterAddRemove/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Security;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatchHeaderFooterAddRemove {
+    public partial class BatchHeaderFooterAddRemove : Form {
+        public List<string> fileList;
+        public List<string> tempFileList;
+        public BatchHeaderFooterAddRemove() {
+            InitializeComponent();
+            fileList = new List<string>();
+            tempFileList = new List<string>();
+        }
+        private void Button1_Click(object sender, EventArgs e) {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog()) {
+                openFileDialog.RestoreDirectory = true;
+                if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                    fileList.Add(openFileDialog.FileName);
+                }
+            }
+            fileList = fileList.Distinct().ToList();
+            listBox1.Items.Clear();
+            for (int i = 0; i < fileList.Count; i++) {
+                listBox1.Items.Add(fileList[i]);
+            }
+        }
+
+        public void ListBox1_SelectedIndexChanged(object sender, EventArgs e) {
+        }
+
+        private void Button2_Click(object sender, EventArgs e) {
+            FolderBrowserDialog Klasor = new FolderBrowserDialog();
+            Klasor.ShowDialog();
+            if (Klasor.SelectedPath != "") {
+                string extension;
+                if (textBox1.Text == "") {
+                    extension = "*";
+                } else {
+                    extension = textBox1.Text;
+                }
+                string[] fileArray = Directory.GetFiles(Klasor.SelectedPath, "*." + extension, SearchOption.AllDirectories);
+                for (int i = 0; i < fileArray.Length; i++) {
+                    fileList.Add(fileArray[i]);
+                }
+            }
+            fileList = fileList.Distinct().ToList();
+            listBox1.Items.Clear();
+            for (int i = 0; i < fileList.Count; i++) {
+                listBox1.Items.Add(fileList[i]);
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e) {
+            FolderBrowserDialog Klasor = new FolderBrowserDialog();
+            Klasor.ShowDialog();
+            if (Klasor.SelectedPath != "") {
+                string extension;
+                if (textBox1.Text == "") {
+                    extension = "*";
+                } else {
+                    extension = textBox1.Text;
+                }
+                string[] fileArray = Directory.GetFiles(Klasor.SelectedPath, "*." + extension, SearchOption.TopDirectoryOnly);
+                for (int i = 0; i < fileArray.Length; i++) {
+                    fileList.Add(fileArray[i]);
+                }
+            }
+            fileList = fileList.Distinct().ToList();
+            listBox1.Items.Clear();
+            for (int i = 0; i < fileList.Count; i++) {
+                listBox1.Items.Add(fileList[i]);
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e) {
+            string text = textBox1.Text;
+        }
+        private void TextBox2_TextChanged(object sender, EventArgs e) {
+            string text = textBox2.Text;
+        }
+        private void TextBox5_TextChanged(object sender, EventArgs e) {
+            string text = textBox5.Text;
+        }
+        private void TextBox3_TextChanged_1(object sender, EventArgs e) {
+            string text = textBox3.Text;
+        }
+        private void TextBox4_TextChanged(object sender, EventArgs e) {
+            string text = textBox4.Text;
+        }
+
+        private void Button5_Click(object sender, EventArgs e) {
+            if (fileList.Count == 0) {
+                MessageBox.Show("No files selected.");
+            } else {
+                if (textBox2.Text == "") {
+                    textBox2.Text = "0";
+                }
+                if (textBox5.Text == "") {
+                    textBox5.Text = "0";
+                }
+                int successful = 0;
+                int failed = 0;
+                for (int i = 0; i < fileList.Count; i++) {
+                    byte[] raw = File.ReadAllBytes(fileList[i]);
+                    if (Int32.Parse(textBox2.Text) + Int32.Parse(textBox5.Text) >= raw.Length) {
+                        MessageBox.Show(fileList[i] + " is too small to remove " + (Int32.Parse(textBox2.Text) + Int32.Parse(textBox5.Text)) + " bytes from.");
+                        failed++;
+                    } else {
+                        raw = raw.Skip(Int32.Parse(textBox2.Text)).ToArray();
+                        byte[] temp = new byte[raw.Length - Int32.Parse(textBox5.Text)];
+                        Array.Copy(raw, temp, raw.Length - Int32.Parse(textBox5.Text));
+                        byte[] addBeginning = Encoding.ASCII.GetBytes(textBox4.Text);
+                        byte[] addEnding = Encoding.ASCII.GetBytes(textBox3.Text);
+                        byte[] allByteArray = new byte[addBeginning.Length + temp.Length + addEnding.Length];
+
+                        System.Buffer.BlockCopy(addBeginning, 0, allByteArray, 0, addBeginning.Length);
+                        System.Buffer.BlockCopy(temp, 0, allByteArray, addBeginning.Length, temp.Length);
+                        System.Buffer.BlockCopy(addEnding, 0, allByteArray, addBeginning.Length + temp.Length, addEnding.Length);
+
+                        if (textBox6.Text == "") {
+                            if (textBox7.Text == "") {
+                                File.WriteAllBytes(Path.GetDirectoryName(fileList[i]) + "/" + Path.GetFileName(fileList[i]), allByteArray);
+                            } else {
+                                File.WriteAllBytes(Path.GetDirectoryName(fileList[i]) + "/" + Path.GetFileNameWithoutExtension(fileList[i]) + "." + textBox7.Text, allByteArray);
+                            }
+                        } else {
+                            if (textBox7.Text == "") {
+                                File.WriteAllBytes(textBox6.Text + "/" + Path.GetFileName(fileList[i]), allByteArray);
+                            } else {
+                                File.WriteAllBytes(textBox6.Text + "/" + Path.GetFileNameWithoutExtension(fileList[i]) + "." + textBox7.Text, allByteArray);
+                            }
+                        }
+                        successful++;
+                    }
+                    MessageBox.Show(failed + " failed " + successful + " successful conversions.");
+                }
+            }
+        }
+        private void Button6_Click(object sender, EventArgs e) {
+            FolderBrowserDialog Klasor = new FolderBrowserDialog();
+            Klasor.ShowDialog();
+            if (Klasor.SelectedPath != "") {
+                textBox6.Text = Klasor.SelectedPath;
+            }
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e) {
+            string text = textBox6.Text;
+        }
+
+        private void TextBox7_TextChanged(object sender, EventArgs e) {
+            string text = textBox7.Text;
+        }
+
+        private void Button4_Click(object sender, EventArgs e) {
+            if (listBox1.SelectedItems.Count > 0) {
+                tempFileList.Clear();
+                for (int i = 0; i < fileList.Count; i++) {
+                    tempFileList.Add(fileList[i]);
+                }
+                fileList.Clear();
+                int[] selected = new int[listBox1.SelectedItems.Count];
+                for (int y = 0; y < listBox1.SelectedItems.Count; y++) {
+                    selected[y] = listBox1.Items.IndexOf(listBox1.SelectedItems[y]);
+                }
+                for (int i = 0; i < tempFileList.Count; i++) {
+                    bool toBeDeleted = false;
+                    for(int y = 0; y < selected.Length; y++) {
+                        if(i == selected[y]) {
+                            toBeDeleted = true;
+                        }
+                    }
+                    if(!toBeDeleted) {
+                        fileList.Add(tempFileList[i]);
+                    }
+                    listBox1.Items.Clear();
+                    for (int x = 0; x < fileList.Count; x++) {
+                        listBox1.Items.Add(fileList[x]);
+                    }
+                }
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-BatchHeaderFooterAddRemove
+# BatchHeaderFooterAddRemove
+Batch add and or remove headers an or footers. All at once!
+
+# Documentation
+"Add files" will open a file selection dialog to add a file. _It will not respect the "(only with the extension )" setting_ next to the "Add files from a folder" buttons.
+
+"Add files from a folder (with subfolders)" buttons will respect the "(only with the extension )" preference if said section is not empty and add files from a selected folder to the list. It will search into subfolders while looking for files.
+
+"Add files from a folder (with subfolders)" buttons will respect the "(only with the extension )" preference if said section is not empty and add files from a selected folder to the list. It will _not_ search into subfolders while looking for files.
+
+"Remove selected" will remove the selected files from the "File List:".
+
+_The text boxes in the "Remove" section of the UI will default to "0" if empty, meaning that will not be applied to files._
+
+_The text boxes in the "Add" section of the UI will default to an empty string ("") if empty, meaning that will not be applied to files._
+
+When clicked on "Start", it will copy each file in the list individually to a temporary byte array, remove specified amount of bytes both from the beginning of the file (header) and end of the file (footer). It will then insert the specified strings converted to hexadecimal bytes (ASCII) both to the beginning of the file (as a header) and to the ending of the file (as a footer).
+
+_Windows Clipboard does not respect line break characters, so those characters will be replaced with a "00" (null). It's a limitation I can not do anything about._
+
+It will output the files to their original locations if the "Output files to:" section is empty.
+
+It will output the files with the "with the extension" next to the "Output files to:" section is not empty.
+
+_If  "Output files to:" and the "with the extension" section next to the former are both empty, it will overwrite the original file._
